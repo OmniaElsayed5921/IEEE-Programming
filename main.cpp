686527a88f4f44ae723fe7921c3297da6e209768
Omnia Elsayed Gaballah <124888667+OmniaElsayed5921@users.noreply.github.com>
2023-03-25 21:08:50 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+struct node{
+  int data;
+  node*next ;
+};
+node*head = NULL ;
+void insert_Node( int value ){
+ node* newnode = new node ;
+ newnode->data = value ;
+ newnode->next= NULL;
+
+ if( head == NULL ){
+   head = newnode ;
+    newnode->next = NULL;
+ }
+  else{
+   node*temp = head;
+   while( temp->next != NULL ){
+    temp = temp->next;
+   }
+  temp->next = newnode;
+   newnode->next = NULL;
+ }
+}
+void insertNode(int value){
+  node* newNode;
+  newNode = new node ;
+  newNode->data = value ;
+  if( head == NULL){
+    head= newNode;
+    newNode->next = NULL;
+  }
+ else{
+    node * last = head ;
+    while( last -> next != NULL ){
+        last=last->next;
+    }
+    last->next= newNode;
+    newNode->next = NULL;
+ }
+}
+void display(){
+  node*current = head;
+  while( current!= NULL ){
+    cout << current-> data << " " ;
+    current=current->next;
+  }
+}
+void reverse_linkedlist(){
+     node* current = head ;
+     node * previouse = NULL;
+     node * Next = current->next ;
+     while( Next!= NULL){
+        Next = current->next ;
+        current->next = previouse ;
+        previouse=current;
+        current=Next;
+     }
+     head= previouse;
+}
+int main()
+{
+    int t ; cin >>  t ;
+    for( int i=1 ; i<=t ; i++ ){
+     int n ; cin >> n ;
+     for( int j=0 ; j<n ; j++ ){
+        int val ; cin >> val;
+        insert_Node(val);
+     }
+     reverse_linkedlist();
+     display();
+     cout << endl;
+    }
+    return 0;
+}
